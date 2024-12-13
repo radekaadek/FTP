f32e0c6cf60f1161ae17f3dd7a3d632fc91131af
radekaadek <radekaadek@gmail.com>
2024-12-13 09:51:51 +0100

    class color

--- zad_laspy/main.py
+++ zad_laspy/main.py
@@ -1,5 +1,8 @@
 import laspy
 import numpy as np
+import open3d as o3d
+import matplotlib.pyplot as plt
+
 # Odczyt pliku LAS
 las = laspy.read("79529_1503958_M-34-5-C-b-4-4-3-4.laz")
 # Ekstrakcja współrzędnych
@@ -67,16 +70,49 @@ def point_extraction_based_on_the_class(las, class_type):
         ground_points = las.points[ground_only]
         return ground_points
 
-# 1. Create a new header
-my_data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-header = laspy.LasHeader(point_format=3, version="1.2")
-header.offsets = np.min(my_data, axis=0)
-header.scales = np.array([0.1, 0.1, 0.1])
-# 3. Create a LasWriter and a point record, then write it
-with laspy.open("somepath.las", mode="w", header=header) as writer:
-    point_record = laspy.ScaleAwarePointRecord.zeros(my_data.shape[0],
-header=header)
-    point_record.x = my_data[:, 0]
-    point_record.y = my_data[:, 1]
-    point_record.z = my_data[:, 2]
-    writer.write_points(point_record)
+def manualne_przycinanie_chmury_punktów(chmura_punktow):
+    print("Manualne przycinanie chmury punktów")
+    print("Etapy przetwarzania danych:")
+    print(" (0) Manualne zdefiniowanie widoku poprzez obrót myszka lub:")
+    print(" (0.1) Podwójne wciśnięcie klawisza X - zdefiniowanie widoku ortogonalnego względem osi X")
+    print(" (0.2) Podwójne wciśnięcie klawisza Y - zdefiniowanie widoku ortogonalnego względem osi Y")
+    print(" (0.3) Podwójne wciśnięcie klawisza Z - zdefiniowanie widoku ortogonalnego względem osi Z")
+    print(" (1) Wciśnięcie klawisza K - zmiana na tryb rysowania")
+    print(" (2.1) Wybór zaznaczenia poprzez wciśnięcie lewego przycisku myszy i interaktywnego narysowania prostokąta lub")
+    print(" (2.2) przytrzymanie przycisku ctrl i wybór wierzchołków poligonu lewym przyciskiem myszy")
+    print(" (3) Wciśnięcie klawisza C - wybór zaznaczonego fragmentu chmury punktów i zapis do pliku")
+    print(" (4) Wciśnięcie klawisza F - powrót do interaktywnego wyświetlania chmury punktów")
+    #o3d.visualization.draw_geometries_with_editing(self, window_name='Open3D', width=1920, height=1080, left=50, top=50, visible=True)
+    o3d.visualization.draw_geometries_with_editing([chmura_punktow],window_name='Przycianie chmury punktow')
+
+
+x, y, z = las.x, las.y, las.z
+las_points = np.vstack((x,y,z)).transpose()
+las_colors = np.vstack((r,g,b)).transpose()
+chmura_punktow = o3d.geometry.PointCloud()
+chmura_punktow.points = o3d.utility.Vector3dVector(las_points)
+# o3d.visualization.draw_geometries_with_editing([chmura_punktow],window_name='Przycianie chmury punktow')
+
+# get only points that are buildings
+buildings = las[las.classification == 6]
+vegetation = las[np.logical_or.reduce((las.classification == 3, las.classification == 4, las.classification == 5))]
+ground = las[las.classification == 2]
+
+print(len(buildings), len(vegetation), len(ground))
+
+lens = np.array([len(buildings), len(vegetation), len(ground)])
+plt.bar(np.arange(3), lens)
+# add labels
+plt.xticks([0, 1, 2], ['budynki', 'roślinność', 'grunt'])
+plt.show()
+
+# color chmura by classification
+red = np.array([255, 0, 0])
+green = np.array([0, 255, 0])
+blue = np.array([0, 0, 255])
+colors = np.zeros_like(las_colors)
+colors[las.classification == 6] = red
+colors[np.logical_or.reduce((las.classification == 3, las.classification == 4, las.classification == 5))] = green
+colors[las.classification == 2] = blue
+chmura_punktow.colors = o3d.utility.Vector3dVector(colors)
+o3d.visualization.draw_geometries_with_editing([chmura_punktow],window_name='Przycianie chmury punktow')
