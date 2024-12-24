24d080122e59942d2956d253ff0a4cdabc1564f5
radekaadek <radekaadek@gmail.com>
2024-12-24 12:52:51 +0100

    wizzardv2

--- 3/p1.py
+++ 3/p1.py
@@ -19,11 +19,13 @@
 
 import Metashape
 import os
+import PyQt6
 from PyQt6.QtWidgets import *
 
 app: Metashape.Application = Metashape.Application()
 doc: Metashape.Document = app.document
 
+image_types = [".jpg", ".jpeg", ".tif", ".tiff"]
 
 def get_path(path):
     if os.path.exists(path):
@@ -34,12 +36,89 @@ def get_path(path):
 def find_files(folder, types):
     return [entry.path for entry in os.scandir(folder) if (entry.is_file() and os.path.splitext(entry.name)[1].lower() in types)]
 
+def getMarker(chunk, Label):
+    for marker in chunk.markers:
+        if marker.label == Label:
+            return marker
+    return None
+ 
+def getCamera(chunk, Label):
+    for camera in chunk.cameras:
+        if camera.label == Label:
+            return camera
+    return None
+
+def export_markers():
+    chunk = doc.chunk
+    file_name = app.getSaveFileName('Export traces', filter="*.txt")
+    if len(file_name) == 0:
+        return
+    with open(file_name, 'w') as f:
+        for marker in chunk.markers:
+            if marker.type == Metashape.Marker.Type.Regular:
+                for camera in chunk.cameras:
+                    if camera.enabled == 1:
+                        Point2D = marker.projections[camera]
+                        ReprojectionError = []
+                        if Point2D is not None:
+                            ReprojectionError = camera.project(marker.position)-marker.projections[camera].coord[:-1]
+                            if ReprojectionError is not None:
+                                data = (
+                                    f"{marker.label} {camera.label} "
+                                    f"{ReprojectionError[0]} {ReprojectionError[1]} "
+                                    f"{marker.projections[camera].coord[0]} {marker.projections[camera].coord[1]}\n"
+                                )
+                                print(data)
+                                f.write(data)
+ 
+def import_markers():
+    chunk = doc.chunk
+    name = app.getOpenFileName('Import traces',filter="*.txt")
+    if len(name) == 0:
+        return
+    File = open(name, "r")
+    content = File.readlines()
+    for line in content:
+        MarkerLabel, CameraLabel, ErrorX, ErrorY, XFeatureCoordinates, YFeatureCoordinates = line.split()
+        marker = getMarker(chunk, MarkerLabel)
+        if not marker:
+            marker = chunk.addMarker()
+            marker.reference.accuracy = Metashape.Vector([0.03,0.03,0.05])
+            marker.label = MarkerLabel
+        camera = getCamera(chunk, CameraLabel)
+        if not camera:
+            print(CameraLabel + " camera not found in project")
+            continue
+        marker.projections[camera] = Metashape.Marker.Projection(Metashape.Vector([float(XFeatureCoordinates), float(YFeatureCoordinates)]), True)
+    File.close()
+
+def get_photos():
+    chunk = doc.chunk
+    if not chunk:
+        chunk = doc.addChunk()
+    photo_folder = app.getExistingDirectory("Please select the folder of photos to load.")
+    if not photo_folder:
+        return
+    crs = app.getCoordinateSystem()
+
+
+    photos = find_files(photo_folder, image_types)
+    chunk.addPhotos(photos)
+    for camera in chunk.cameras:
+        camera.reference.location = crs.project(chunk.crs.unproject(camera.reference.location))
+    chunk.crs = crs
+    chunk.updateTransform()
+
 def wizard():
     qt_app = QApplication.instance()  # Get the existing QApplication instance if it exists
     if not qt_app:  # If no instance exists, create a new one
         qt_app = QApplication([])
     window = QWidget()
     layout = QVBoxLayout()
+
+    # add a big label with the title
+    title_label = QLabel("Agisoft Metashape Pro Wizard")
+    layout.addWidget(title_label)
 
     # Button to get photos
     btn_get_photos = QPushButton("Get Photos")
@@ -62,9 +141,11 @@ def wizard():
 
     gen_presel_checkbox = QCheckBox("Enable generic preselection")
     layout.addWidget(gen_presel_checkbox)
+    gen_presel_checkbox.setChecked(True)
 
     ref_presel_checkbox = QCheckBox("Enable reference preselection")
     layout.addWidget(ref_presel_checkbox)
+    ref_presel_checkbox.setChecked(True)
 
     reset_align_checkbox = QCheckBox("Reset current alignment")
     layout.addWidget(reset_align_checkbox)
@@ -109,6 +190,18 @@ def wizard():
         downscale_int = depth_accuracy_combo.currentData()
         chunk.buildDepthMaps(downscale=downscale_int)
 
+
+    btn_depth_maps = QPushButton("Generate Depth Maps")
+    btn_depth_maps.clicked.connect(build_depth_maps)
+    layout.addWidget(btn_depth_maps)
+
+    btn_point_colors = QCheckBox("Calculate Point Colors")
+    layout.addWidget(btn_point_colors)
+    btn_point_colors.setChecked(True)
+    btn_point_confidence = QCheckBox("Calculate Point Confidence")
+    layout.addWidget(btn_point_confidence)
+    btn_point_confidence.setChecked(True)
+
     def build_point_cloud():
         chunk = doc.chunk
         if not chunk:
@@ -118,99 +211,72 @@ def wizard():
         name = None
         if save_cloud:
             name = app.getSaveFileName('Export point cloud path', filter="*.las")
-        chunk.buildPointCloud(point_colors=True)
+        chunk.buildPointCloud(point_colors=btn_point_colors.isChecked(), point_confidence=btn_point_confidence.isChecked())
         if save_cloud and name is not None:
             chunk.exportPointCloud(name)
 
 
-    btn_depth_maps = QPushButton("Generate Depth Maps")
-    btn_depth_maps.clicked.connect(build_depth_maps)
-    layout.addWidget(btn_depth_maps)
 
     btn_build_point_cloud = QPushButton("Build Point Cloud")
     btn_build_point_cloud.clicked.connect(build_point_cloud)
     layout.addWidget(btn_build_point_cloud)
 
+    face_count_label = QLabel("Model face count target:")
+    layout.addWidget(face_count_label)
+
+    face_counts = {0: "Low", 1: "Medium", 2: "High", 3: "Custom"}
+    face_count_combo = QComboBox()
+    for key, value in face_counts.items():
+        face_count_combo.addItem(value, key)
+    layout.addWidget(face_count_combo)
+
+    save_model = QCheckBox("Save generated model")
+    layout.addWidget(save_model)
+
+    def build_model():
+        chunk = doc.chunk
+        if not chunk:
+            app.messageBox("Error, please select a chunk with depth maps first.")
+            return
+        name = None
+        if save_model.isChecked():
+            name = app.getSaveFileName('Export model', filter="*.obj")
+
+        face_count_int = face_count_combo.currentData()
+        if face_count_int == 3:  # Custom face count
+            face_num = -1
+            while face_num < 1:
+                face_num = app.getInt("Please enter the desired number of faces")
+                if face_num is None:
+                    return
+                if face_num < 1:
+                    app.messageBox("Invalid input, please enter a positive integer")
+            face_count = Metashape.FaceCount.CustomFaceCount(face_count=face_count_int, face_size=face_num)
+        else:
+            face_count = {0: Metashape.FaceCount.LowFaceCount, 1: Metashape.FaceCount.MediumFaceCount, 2: Metashape.FaceCount.HighFaceCount}[face_count_int]
+
+        chunk.buildModel(face_count=face_count)
+        if save_model.isChecked() and name is not None:
+            chunk.exportModel(name)
 
     # Button to build model
     btn_build_model = QPushButton("Build Model")
     btn_build_model.clicked.connect(build_model)
     layout.addWidget(btn_build_model)
 
+    btn_import_markers = QPushButton("Import Markers")
+    btn_import_markers.clicked.connect(import_markers)
+    layout.addWidget(btn_import_markers)
+
+    btn_export_markers = QPushButton("Export Markers")
+    btn_export_markers.clicked.connect(export_markers)
+    layout.addWidget(btn_export_markers)
+
     window.setLayout(layout)
     window.show()
     qt_app.exec()
 
 
-def get_photos():
-    chunk = doc.chunk
-    if not chunk:
-        chunk = doc.addChunk()
-    photo_folder = app.getExistingDirectory("Please select the folder of photos to load.")
-    if not photo_folder:
-        return
-    crs = app.getCoordinateSystem()
-
-
-    photos = find_files(photo_folder, image_types)
-    chunk.addPhotos(photos)
-    for camera in chunk.cameras:
-        camera.reference.location = crs.project(chunk.crs.unproject(camera.reference.location))
-    chunk.crs = crs
-    chunk.updateTransform()
-
-def build_cloud():
-    pass
-
-def build_model():
-    # get depth map
-
-    face_counts = Metashape.FaceCount
-    num_to_face = {0: face_counts.LowFaceCount, 1: face_counts.MediumFaceCount, 2: face_counts.HighFaceCount, 3: face_counts.CustomFaceCount}
-    cnt = -1
-    while cnt not in num_to_face.keys():
-        cnt = app.getInt(f"Please enter the model's desired face count\n"
-                         "0: Low face count\n"
-                         "1: Medium face count\n"
-                         "2: High face count\n"
-                         "3: Custom face count")
-        if cnt is None:
-            return
-        if cnt not in num_to_face.keys():
-            app.messageBox(f"Invalid input, please enter a valid integer from:\n{num_to_face.keys()}")
-    face_cnt = num_to_face[cnt]
-
-    chunk = doc.chunk
-    save_model = app.getBool("Save the created model?")
-
-
-    chunk = doc.chunk
-    if face_cnt == face_counts.CustomFaceCount:
-        face_num = -1
-        while face_num < 1:
-            face_num = app.getInt("Please enter the desired number of faces")
-            if face_num is None:
-                return
-            if face_num < 1:
-                app.messageBox("Invalid input, please enter a positive integer")
-        face_cnt = face_counts.CustomFaceCount(face_count=face_cnt, face_size=face_num)
-    else:
-        chunk.buildModel(face_count=face_cnt)
-    if save_model:
-        chunk.exportModel(app.getSaveFileName('Export model', filter="*.obj"))
-
-
-image_types = [".jpg", ".jpeg", ".tif", ".tiff"]
-
-
-app.removeMenuItem("Get Photos")
-app.addMenuItem("Get Photos", get_photos)
-
-app.removeMenuItem("Generate Depth Maps and Build Point Cloud")
-app.addMenuItem("Generate Depth Maps and Build Point Cloud", build_cloud)
-
-app.removeMenuItem("Build Model")
-app.addMenuItem("Build Model", build_model)
 
 app.removeMenuItem("Wizard")
 app.addMenuItem("Wizard", wizard)
